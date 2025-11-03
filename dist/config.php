b0e28aaa9c606e25e624666b433dda6fd79320de
Rusiann <Rusiann7@protonmail.com>
2025-11-03 19:19:12 +0800

    Add built site

---
+++ dist/config.php
@@ -0,0 +1,35 @@
+<?php //config file
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header('Access-Control-Max-Age: 86400');
+header('Content-Type: application/json; charset=utf-8');
+
+require __DIR__ . '../../vendor/autoload.php';
+
+if ($_SERVER['REQUEST_METHOD'] !== 'OPTIONS') {
+    header('Content-Type: application/json; charset=utf-8');
+}
+
+$epass = 'gowo rwob sjza ipdp';
+$yourApiKey = 'AIzaSyAZ4TMBO6PM1VMuJ7OT638zQp93aRjL37A';
+
+$host ="rusiann7.helioho.st";
+$user ="rusiann7_ruspin";
+$password="857k9dg#N";
+$dbname="rusiann7_pageinfo";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if($conn->connect_error) {
+    http_response_code(500);
+    die(json_encode(["success" => false, "error" => "DB Connection failed"]));
+}
+
+$conn->set_charset("utf8mb4");
+$conn->query("SET NAMES 'utf8mb4'");
